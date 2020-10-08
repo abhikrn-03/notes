cb7c5b31dc120ed93a6083797c310f07dec077aa
abhikrn-03 <abhik333333@gmail.com>
2020-10-09 04:15:16 +0530

    Adding a note

--- app.js
+++ app.js
@@ -1,6 +1,58 @@
 const chalk = require('chalk');
-const note = require('./notes.js');
+const { describe, argv } = require('yargs');
+const yargs = require('yargs');
+const notes = require('./notes.js');
 
-const note1 = note();
-console.log(chalk.blue.bgRed.bold(note1));
-console.log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+// Adding a note
+yargs.command({
+    command: 'add',
+    describe: 'add a note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        notes.addNote
+        console.log('Title: ' + chalk.blue(argv.title));
+        console.log('Body: ' + chalk.blue(argv.body));
+    }
+});
+
+// Removing a note
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a note');
+    }
+});
+
+// Reading a note
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note');
+    }
+});
+
+
+// Listing the notes
+yargs.command({
+    command: 'list',
+    describe: 'List the notes',
+    handler: function () {
+        console.log('Listing the notes');
+    }
+});
+
+yargs.parse();
+
--- notes.js
+++ notes.js
@@ -2,4 +2,15 @@ const note = function(){
     return "My notes are the best.";
 }
 
-module.exports = note;
+const addNote = function(title, body){
+
+}
+
+const loadNote = function(){
+
+}
+
+module.exports = {
+    addNote: addNote,
+    loadNote: loadNote
+}
